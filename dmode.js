6295b2d018f2127bd2925f6bff0367ff5cf53cd7
TBearCode <111528297+TBearCode@users.noreply.github.com>
2024-02-14 20:42:09 -0700

    Update dmode.js

--- dmode.js
+++ dmode.js
@@ -1,5 +1,5 @@
 //implements dark mode with any keydown event by adding dark class to the root html element
-html = document.querySelector("html");
+const html = document.querySelector("html");
 document.addEventListener('keydown', e=> {
 	html.classList.toggle("dark");
 })
